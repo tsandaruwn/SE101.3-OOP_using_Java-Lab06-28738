6adc996d25deca4e962d84ce40e6dfb104535378
Thilina Sandaruwan <125427014+tsandaruwn@users.noreply.github.com>
2023-07-31 23:55:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    Cricle h1=new Cricle(7);
+    h1.calArea();
+    h1.display();
+
+    Rectangle h2=new Rectangle(50,20);
+    h2.calArea();
+    h2.display();
+    }
+}
